85f817f5cbfa33bfbcd72488c7f9fc27a3d1fec8
최민재 <cmjcmj1230@gmail.com>
2022-11-22 10:32:06 +0900

    Add: Test
    민성이를 가르쳐 주고 있습니다... 어휴

--- app/src/main/java/kr/minjae/develop/jjiggijjiggi/MainActivity.kt
+++ app/src/main/java/kr/minjae/develop/jjiggijjiggi/MainActivity.kt
@@ -2,10 +2,13 @@ package kr.minjae.develop.jjiggijjiggi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("Test", "onCreate: Test")
     }
 }
