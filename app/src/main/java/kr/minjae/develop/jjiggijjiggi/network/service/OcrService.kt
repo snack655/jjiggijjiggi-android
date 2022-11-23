75d86699a20b2321edc25cbd344fa228d6b75dd9
최민재 <cmjcmj1230@gmail.com>
2022-11-23 15:43:24 +0900

    Add: Ocr service

--- app/src/main/java/kr/minjae/develop/jjiggijjiggi/network/service/OcrService.kt
+++ app/src/main/java/kr/minjae/develop/jjiggijjiggi/network/service/OcrService.kt
@@ -10,10 +10,10 @@ import retrofit2.http.POST
 interface OcrService {
 
     @POST("general")
-    fun getTextInImage(
+    suspend fun getTextInImage(
         @Header("Content-Type") contentType: String = "application/json",
         @Header("X-OCR-SECRET") xOcrSecret: String = "eGNzdmxDTHFSR3p0YUdkZ0lwT0VPdlhCQXVNTWJ0dUE=",
         @Body ocrRequest: OcrRequest
-    ): Call<OcrResponse>
+    ): OcrResponse
 
 }
